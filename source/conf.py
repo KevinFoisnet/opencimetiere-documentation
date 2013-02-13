62f9470f5436bf2524d6bdb0504cab5d03a2e504
Florent Michon <fmichon@atreal.fr>
2013-02-13 13:25:57 +0000

    Suppression de l'affichage du lien pour montrer la source du rst dans la génération html

--- source/conf.py
+++ source/conf.py
@@ -48,7 +48,7 @@ copyright = u'2013, openMairie'
 # built documents.
 #
 # The short X.Y version.
-version = '1.0.0'
+version = '3.x'
 # The full version, including alpha/beta/rc tags.
 release = '3.x'
 
@@ -147,7 +147,7 @@ html_static_path = ['_static']
 #html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
+html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 #html_show_sphinx = True
