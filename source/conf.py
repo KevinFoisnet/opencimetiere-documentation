72e63af4663cd2e3b4757d0f0f8e47c79a53d097
Florent Michon <fmichon@atreal.fr>
2014-01-15 16:19:33 +0100

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -40,8 +40,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'openCimetière'
-copyright = u'2004-2013, openMairie'
+project = u'openCimetière 3.0'
+copyright = u'2004-2014, openMairie'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
